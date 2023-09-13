9e92e02678c2ee3324abbdd8994ac64173e46508
Eivind Furuberg <eivind@furuberg.dev>
2023-09-13 23:09:15 +0200

    styling cs rating

--- components/CSRating.vue
+++ components/CSRating.vue
@@ -3,25 +3,53 @@ defineProps<{ rating: number }>()
 </script>
 
 <template>
-  <div v-if="rating >= 30000" class="h-full w-full text-center bg-[#674c00] text-[#fff620]"><span class="text-xl">{{
-    rating.toString().slice(0, 2)
-  }}</span>,<span>{{ rating.toString().slice(2, 5) }}</span></div>
-  <div v-else-if="rating >= 25000" class="h-full w-full text-center bg-[#581618] text-[#f86d68]"><span class="text-xl">{{
-    rating.toString().slice(0, 2)
-  }}</span>,<span>{{ rating.toString().slice(2, 5) }}</span></div>
-  <div v-else-if="rating >= 20000" class="h-full w-full text-center bg-[#590f5c] text-[#f54df8]"><span class="text-xl">{{
-    rating.toString().slice(0, 2)
-  }}</span>,<span>{{ rating.toString().slice(2, 5) }}</span></div>
-  <div v-else-if="rating >= 15000" class="h-full w-full text-center bg-[#46285e] text-[#e08bf3]"><span class="text-xl">{{
-    rating.toString().slice(0, 2)
-  }}</span>,<span>{{ rating.toString().slice(2, 5) }}</span></div>
-  <div v-else-if="rating >= 10000" class="h-full w-full text-center bg-[#252757] text-[#949fff]"><span class="text-xl">{{
-    rating.toString().slice(0, 2)
-  }}</span>,<span>{{ rating.toString().slice(2, 5) }}</span></div>
-  <div v-else-if="rating >= 5000" class="h-full w-full text-center bg-[#284555] text-[#baf6ff]"><span class="text-xl">{{
-    rating.toString().slice(0, 2)
-  }}</span>,<span>{{ rating.toString().slice(2, 5) }}</span></div>
-  <div v-else class="h-full w-full text-center bg-[#414850] text-[#e6f3fc]"><span class="text-xl">{{
-    rating.toString().slice(0, 2)
-  }}</span>,<span>{{ rating.toString().slice(2, 5) }}</span></div>
+  <div v-if="rating >= 30000" class="relative h-full w-full my-1 text-[gold]">
+    <img class="h-5/6 w-5/6" src="/images/cs-rating-7.svg" :alt="rating.toString()">
+    <div class="absolute top-0 left-5">
+      <span class="text-2xl">{{ rating.toString().slice(0, 2) }},</span>
+      <span class="text-md">{{ rating.toString().slice(2, 5) }}</span>
+    </div>
+  </div>
+  <div v-else-if="rating >= 25000" class="relative h-full w-full my-1 text-[#EB4B4B]">
+    <img class="h-5/6 w-5/6" src="/images/cs-rating-6.svg" :alt="rating.toString()">
+    <div class="absolute top-0 left-5">
+      <span class="text-2xl">{{ rating.toString().slice(0, 2) }},</span>
+      <span class="text-md">{{ rating.toString().slice(2, 5) }}</span>
+    </div>
+  </div>
+  <div v-else-if="rating >= 20000" class="relative h-full w-full my-1 text-[#F03CFF]">
+    <img class="h-5/6 w-5/6" src="/images/cs-rating-5.svg" :alt="rating.toString()">
+    <div class="absolute top-0 left-5">
+      <span class="text-2xl">{{ rating.toString().slice(0, 2) }},</span>
+      <span class="text-md">{{ rating.toString().slice(2, 5) }}</span>
+    </div>
+  </div>
+  <div v-else-if="rating >= 15000" class="relative h-full w-full my-1 text-[#C166FF]">
+    <img class="h-5/6 w-5/6" src="/images/cs-rating-4.svg" :alt="rating.toString()">
+    <div class="absolute top-0 left-5">
+      <span class="text-2xl">{{ rating.toString().slice(0, 2) }},</span>
+      <span class="text-md">{{ rating.toString().slice(2, 5) }}</span>
+    </div>
+  </div>
+  <div v-else-if="rating >= 10000" class="relative h-full w-full my-1 text-[#6A7DFF]">
+    <img class="h-5/6 w-5/6" src="/images/cs-rating-3.svg" :alt="rating.toString()">
+    <div class="absolute top-0 left-5">
+      <span class="text-2xl">{{ rating.toString().slice(0, 2) }},</span>
+      <span class="text-md">{{ rating.toString().slice(2, 5) }}</span>
+    </div>
+  </div>
+  <div v-else-if="rating >= 5000" class="relative h-full w-full my-1 text-[#8CC6FF]">
+    <img class="h-5/6 w-5/6" src="/images/cs-rating-2.svg" :alt="rating.toString()">
+    <div class="absolute top-0 left-5">
+      <span class="text-2xl">{{ rating.toString().slice(0, 1) }},</span>
+      <span class="text-md">{{ rating.toString().slice(1, 4) }}</span>
+    </div>
+  </div>
+  <div v-else class="relative h-full w-full my-1 text-[#B0C3D9]">
+    <img class="h-5/6 w-5/6" src="/images/cs-rating-1.svg" :alt="rating.toString()">
+    <div class="absolute top-0 left-5">
+      <span class="text-2xl">{{ rating.toString().slice(0, 1) }},</span>
+      <span class="text-md">{{ rating.toString().slice(1, 4) }}</span>
+    </div>
+  </div>
 </template>
